45b06c8fbf2b45d417ebe39a352ee2b71b6642ed
tarandeep9988 <tarandeep65singh@gmail.com>
2025-06-24 20:17:55 +0530

    auth routes created

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,10 @@ connectDB();
 app.use("/api/v1/auth", authRoutes)
 
 
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
