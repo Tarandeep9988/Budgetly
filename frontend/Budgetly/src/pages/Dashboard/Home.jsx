9f9260dbe2c3b5527b3acc5343772c4a094e2ceb
tarandeep9988 <tarandeep65singh@gmail.com>
2025-06-26 08:28:41 +0530

    add : signup integration with backend

--- frontend/Budgetly/src/pages/Dashboard/Home.jsx
+++ frontend/Budgetly/src/pages/Dashboard/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import DashboardLayout from '../../components/layouts/DashboardLayout'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <DashboardLayout activeMenu="Dashboard" >
+      <div className='my-5 mx-auto'>
+        Home
+
+      </div>
+    </DashboardLayout>
   )
 }
 
